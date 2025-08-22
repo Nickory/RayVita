c3ff6a59a3ef6727d4e1f5dc89b22aaf44a9c680
Ziheng Wang <104838600+Nickory@users.noreply.github.com>
2025-08-22 12:45:01 +0800

    5.03 下载跟新按钮
    跟新下载跟新按钮

--- app/src/main/java/com/codelab/basiclayouts/ui/screen/profile/DownloadFloatingButton.kt
+++ app/src/main/java/com/codelab/basiclayouts/ui/screen/profile/DownloadFloatingButton.kt
@@ -2,27 +2,58 @@ package com.codelab.basiclayouts.ui.profile
 
 import android.content.Intent
 import android.net.Uri
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.GetApp
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.codelab.basiclayouts.R
+
 
 @Composable
 fun DownloadFloatingButton() {
     val context = LocalContext.current
 
-    FloatingActionButton(
+    ExtendedFloatingActionButton(
         onClick = {
             val intent = Intent(
                 Intent.ACTION_VIEW,
                 Uri.parse("http://47.96.237.130/download")
             )
             context.startActivity(intent)
-        }
-    ) {
-        Text(stringResource(R.string.profile_download))
-    }
+        },
+        icon = {
+            Icon(
+                imageVector = Icons.Filled.GetApp,
+                contentDescription = stringResource(R.string.profile_download_new_version)
+            )
+        },
+        text = { Text(stringResource(R.string.profile_download_new_version)) },
+        modifier = Modifier
+            .clip(RoundedCornerShape(24 ))
+            .background(
+                brush = Brush.horizontalGradient(
+                    listOf(
+                        MaterialTheme.colorScheme.primary,
+                        MaterialTheme.colorScheme.tertiary
+                    )
+                )
+            ),
+        containerColor = Color.Transparent,
+        contentColor = Color.White,
+        elevation = FloatingActionButtonDefaults.elevation(64.dp)
+    )
 }
 
